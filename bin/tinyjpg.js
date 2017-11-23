7e73046e481e8b25939db85e0028fa7dea4e3f13
Kermit-Xuan <kermitlx@outlook.com>
2017-11-24 01:32:56 +0800

    fix command

--- bin/tinyjpg.js
+++ bin/tinyjpg.js
@@ -4,4 +4,4 @@
 require('commander').version(require('../package').version)
 .usage('<command> <options>')
 .command('config', 'add or remove config')
-.command('compress', 'compress images')
+.command('compress', 'compress images').parse(process.argv)
